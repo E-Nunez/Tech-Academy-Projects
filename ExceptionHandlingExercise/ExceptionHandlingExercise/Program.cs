0e1dc5723a4bbbea0c74f20ccd47fc9323bdf417
Eric Nuñez <ericnunez95@gmail.com>
2018-07-31 20:56:36 -0700

    C# Exception Handling Exercise

---
+++ ExceptionHandlingExercise/ExceptionHandlingExercise/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandlingExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                List<decimal> numbers = new List<decimal>() { 10, 20, 40, 70, 100 };
+                Console.WriteLine("Pick a number to divide from the list \n (10, 20, 40, 70, 100)");
+                decimal numInput = Convert.ToDecimal(Console.ReadLine());
+                Console.WriteLine();
+
+
+                for (int i = 0; i < numbers.Count; i++)
+                {
+                    decimal result = numbers[i] / numInput;
+                    Console.WriteLine("Result:");
+                    Console.WriteLine(result + "\n");
+                }
+                
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+                return;
+            }
+
+            Console.WriteLine("The Program has emerged from the try/catch block and continued with execution.");
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
