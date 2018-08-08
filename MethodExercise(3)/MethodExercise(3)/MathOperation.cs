7cfdcf586fb9ddd671c49ce076027c8cf6e544f8
Eric Nuñez <ericnunez95@gmail.com>
2018-08-07 21:20:39 -0700

    C# Method Exercise

---
+++ MethodExercise(3)/MethodExercise(3)/MathOperation.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodExercise_3_
+{
+    class MathOperation
+    {
+        public void MathCalc(out int num1)
+        {
+            num1 = 50;
+            Console.WriteLine(num1 + " divided by 2 = " + (num1 / 2));
+
+            Console.WriteLine();
+        }
+
+        public void MathCalc(out int num1, out int num2)
+        {
+            num1 = 2;
+            Console.WriteLine("Enter a number:");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(num2 + " divided by " + num1 + " = " + (num2 / num1));
+
+            Console.WriteLine();
+        }
+
+        public void MathCalc(out int num1, out int num2, out int num3)
+        {
+            Console.WriteLine("Enter a number:");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter another number to multiply with previous number: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            string result = Convert.ToString(num1 * num2);
+            Console.WriteLine("= " + result);
+
+ 
+
+            Console.WriteLine("Enter one more number to divide with the result of last two numbers entered: ");
+            num3 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(result + " divided by " + num3 + " = " + (Convert.ToInt32(result) / num3));
+        }
+    }
+}
---
+++ MethodExercise(3)/MethodExercise(3)/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodExercise_3_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            int num2;
+            int num3;
+            MathOperation divide = new MathOperation();
+            divide.MathCalc(out num);
+            divide.MathCalc(out num, out num2);
+            divide.MathCalc(out num, out num2, out num3);
+            Console.ReadLine();
+            
+        }
+    }
+}
