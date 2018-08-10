9152a46bec14001ddaf3006bea8c94701757169e
Eric Nuñez <ericnunez95@gmail.com>
2018-08-09 21:14:25 -0700

    C# Method Exercise

---
+++ MethodExercise(4)/MethodExercise(4)/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodExercise_4_
+{
+    public class Employee : Person
+    {
+        public int Id { get; set; }
+
+    }
+}
