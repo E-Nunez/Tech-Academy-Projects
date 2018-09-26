188bd0603ddf6f55b7ee9d227376ec0012cb757c
Eric Nuñez <ericnunez95@gmail.com>
2018-09-25 19:06:11 -0700

    C# Logging Exercise

---
+++ Logging/Logging/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Logging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type in a number:");
+            string number = Console.ReadLine();
+            File.WriteAllText(@"C:\Users\ericn\Logs\log.txt", number);
+
+            Console.WriteLine("The number you chose is:");
+            string text = File.ReadAllText(@"C:\Users\ericn\Logs\log.txt");
+            Console.WriteLine(text);
+            Console.ReadLine();
+        }
+    }
+}
